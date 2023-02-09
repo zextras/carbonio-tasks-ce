8ec03c15cefbe4be8b526a34e771cef3a05f9466
Federico Rispo <federico.rispo@zextras.com>
2023-02-09 14:29:49 +0100

    feat: TSK-12 - Add pre-commit, licenses and empty maven project

---
+++ src/test/java/com/zextras/carbonio/tasks/AppTest.java
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package com.zextras.carbonio.tasks;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
