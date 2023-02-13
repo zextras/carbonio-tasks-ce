c255239b5db139e32c764a5bca8416b41a60ce24
Federico Rispo <federico.rispo@zextras.com>
2023-02-13 12:35:46 +0100

    feat: Add health APIs

--- core/src/main/java/com/zextras/carbonio/tasks/rest/controllers/HealthController.java
+++ core/src/main/java/com/zextras/carbonio/tasks/rest/controllers/HealthController.java
@@ -6,11 +6,21 @@ package com.zextras.carbonio.tasks.rest.controllers;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 @Path("/health")
 public interface HealthController {
 
   @GET
+  @Produces("application/json")
   Response health();
+
+  @GET
+  @Path("ready")
+  Response healthReady();
+
+  @GET
+  @Path("live")
+  Response healthLive();
 }
--- core/src/main/java/com/zextras/carbonio/tasks/rest/controllers/HealthControllerImpl.java
+++ core/src/main/java/com/zextras/carbonio/tasks/rest/controllers/HealthControllerImpl.java
@@ -5,10 +5,24 @@
 package com.zextras.carbonio.tasks.rest.controllers;
 
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 public class HealthControllerImpl implements HealthController {
 
+  // TODO: implement the json response
   public Response health() {
-    return Response.ok().build();
+    boolean databaseIsUp = true;
+    return healthReady();
+  }
+
+  public Response healthLive() {
+    return Response.noContent().build();
+  }
+
+  public Response healthReady() {
+    boolean databaseIsUp = true;
+    return databaseIsUp
+        ? Response.noContent().build()
+        : Response.status(Status.BAD_GATEWAY).build();
   }
 }
