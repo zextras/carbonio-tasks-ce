5b93cb953c097f7f45f74025b5f07ba64346dcc5
Federico Rispo <federico.rispo@zextras.com>
2023-02-14 08:19:27 +0100

    feat: TSK-12 - Setup the repository and an empty maven project (#1)
    - Added pre-commit, licenses and empty maven project
    - Added maven-assembly plugin and structured the project in two maven modules:
        - carbonio-tasks-ce-boot
        - carbonio-tasks-ce-core
    
    For now it is a simple HelloWorld

---
+++ boot/src/main/java/com/zextras/carbonio/tasks/Boot.java
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package com.zextras.carbonio.tasks;
+
+public class Boot {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ core/src/main/java/com/zextras/carbonio/tasks/Constants.java
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package com.zextras.carbonio.tasks;
+
+public final class Constants {
+
+  private Constants() {}
+}
