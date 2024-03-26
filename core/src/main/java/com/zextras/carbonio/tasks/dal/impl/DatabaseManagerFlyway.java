ce5754eab36a84d1fb0136b2904883d3f8bdde16
matteogalvagni <matteo.galvagni@zextras.com>
2024-03-26 12:20:24 +0100

    feat: replaced manual db version utility methods with more compact flyway specific ones and updated tests

--- core/src/main/java/com/zextras/carbonio/tasks/dal/impl/DatabaseManagerFlyway.java
+++ core/src/main/java/com/zextras/carbonio/tasks/dal/impl/DatabaseManagerFlyway.java
@@ -6,12 +6,9 @@ package com.zextras.carbonio.tasks.dal.impl;
 
 import com.google.inject.Inject;
 import com.zextras.carbonio.tasks.dal.DatabaseManager;
-import com.zextras.carbonio.tasks.utilities.DatabaseUtils;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import org.flywaydb.core.Flyway;
-import org.flywaydb.core.api.Location;
+import org.flywaydb.core.api.MigrationInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,12 +32,8 @@ public class DatabaseManagerFlyway implements DatabaseManager {
 
   @Override
   public String getDatabaseVersion() {
-    try {
-      ResultSet flywayInfo = fetchFlywayInfo();
-      return flywayInfo.getString("version");
-    } catch (SQLException e) {
-      return "0";
-    }
+    MigrationInfo current = flyway.info().current();
+    return current != null ? flyway.info().current().getVersion().getVersion() : "0";
   }
 
   @Override
@@ -54,51 +47,6 @@ public class DatabaseManagerFlyway implements DatabaseManager {
 
   @Override
   public boolean isDatabaseCorrectVersion() {
-    // this returns if the last database version's script is included in
-    // local migration path (db/migration). if the last version's script is not in path the database
-    // was migrated
-    // to a newer version by another client and this client must be updated.
-    // this is to be sure that no other client has updated db and thus this instance doesn't have
-    // the updated code
-    // and is using old/no more existing tables
-
-    // flyway.info() uses two connections at the same time. setting Constants.Hikari.MAX_POOL_SIZE
-    // to 3 instead
-    // of 2 makes this call work; with MAX_POOL_SIZE = 2 this crashes with an InterruptException.
-    // https://github.com/flyway/flyway/issues/3237
-
-    // since I can't change max pool size here I manually implement "return
-    // !flyway.info().current().getPhysicalLocation().isEmpty();"
-    // using a single connection
-
-    try (ResultSet flywayInfo = fetchFlywayInfo()) {
-      String lastMigrationScript = flywayInfo.getString("script");
-
-      // check if lastMigrationScript is in one of flyway locations
-      Location[] locations =
-          flyway
-              .getConfiguration()
-              .getLocations(); // this will return just one location, db/migration, but ynk
-      for (Location location : locations) {
-        if (DatabaseUtils.isScriptInPath(lastMigrationScript, location.getPath())) return true;
-      }
-      return false;
-    } catch (SQLException e) {
-      return false;
-    }
-  }
-
-  private ResultSet fetchFlywayInfo() throws SQLException {
-    Statement stm = flyway.getConfiguration().getDataSource().getConnection().createStatement();
-    ResultSet rs =
-        stm.executeQuery(
-            "select version, script "
-                + "from flyway_schema_history "
-                + "where success = true "
-                + "order by installed_on desc "
-                + "limit 1");
-
-    rs.next(); // this query returns only one row and should not be empty
-    return rs;
+    return !flyway.info().current().getPhysicalLocation().isEmpty();
   }
 }
