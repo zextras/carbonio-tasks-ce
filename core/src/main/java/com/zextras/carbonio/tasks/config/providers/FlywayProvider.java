f915cbb5c08b4d90fff4594e3a3e0fd324063ce3
Federico Rispo <federico.rispo@zextras.com>
2024-03-26 10:46:33 +0100

    fix: replace HikariDatasource with the Flyway one since it never release the connection

--- core/src/main/java/com/zextras/carbonio/tasks/config/providers/FlywayProvider.java
+++ core/src/main/java/com/zextras/carbonio/tasks/config/providers/FlywayProvider.java
@@ -6,6 +6,7 @@ package com.zextras.carbonio.tasks.config.providers;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
+import com.zaxxer.hikari.HikariDataSource;
 import com.zextras.carbonio.tasks.config.TasksConfig;
 import org.flywaydb.core.Flyway;
 
@@ -19,9 +20,10 @@ public class FlywayProvider implements Provider<Flyway> {
 
   @Override
   public Flyway get() {
+    HikariDataSource dataSource = tasksConfig.getDataSource();
     return Flyway.configure()
-        .dataSource(tasksConfig.getDataSource())
-        .baselineOnMigrate(true) // if schema is not empty create baseline, if it is ignore
+        .dataSource(dataSource.getJdbcUrl(), dataSource.getUsername(), dataSource.getPassword())
+        .baselineOnMigrate(true) // if schema is not empty create baseline, if it is ignored
         .baselineVersion("0")
         .load();
   }
