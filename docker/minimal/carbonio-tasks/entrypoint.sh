d76a0d698e29dfb79637062eb016ce9cc3b46d61
Davide Frison <davide.frison@zextras.com>
2025-11-17 14:19:39 +0100

    chore: add properties only if env set

--- docker/minimal/carbonio-tasks/entrypoint.sh
+++ docker/minimal/carbonio-tasks/entrypoint.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
-cat > /etc/carbonio/tasks/config.properties <<EOF
-carbonio.tasks.host=${CARBONIO_TASKS_HOST}
-carbonio.tasks.port=${CARBONIO_TASKS_PORT}
+echo "" > /etc/carbonio/files/config.properties
 
-carbonio.postgres.host=${CARBONIO_POSTGRES_HOST}
-carbonio.postgres.port=${CARBONIO_POSTGRES_PORT}
+addEnvToProperties() {
+  if [ -n "$2" ];
+  then echo "$1=$2" >> /etc/carbonio/files/config.properties;
+  else echo "$1 is not set. Skipping it.";
+  fi
+}
 
-carbonio.user-management.host=${CARBONIO_USER_MANAGEMENT_HOST}
-carbonio.user-management.port=${CARBONIO_USER_MANAGEMENT_PORT}
+addEnvToProperties "carbonio.tasks.host" "${CARBONIO_TASKS_HOST}"
+addEnvToProperties "carbonio.tasks.port" "${CARBONIO_TASKS_PORT}"
 
-carbonio.service-discover.host=${CARBONIO_SERVICE_DISCOVER_HOST}
-carbonio.service-discover.port=${CARBONIO_SERVICE_DISCOVER_PORT}
-EOF
+addEnvToProperties "carbonio.postgres.host" "${CARBONIO_POSTGRES_HOST}"
+addEnvToProperties "carbonio.postgres.port" "${CARBONIO_POSTGRES_PORT}"
+
+addEnvToProperties "carbonio.user-management.host" "${CARBONIO_USER_MANAGEMENT_HOST}"
+addEnvToProperties "carbonio.user-management.port" "${CARBONIO_USER_MANAGEMENT_PORT}"
+
+addEnvToProperties "carbonio.service-discover.host" "${CARBONIO_SERVICE_DISCOVER_HOST}"
+addEnvToProperties "carbonio.service-discover.port" "${CARBONIO_SERVICE_DISCOVER_PORT}"
 
 JAR=$(ls /app/carbonio-tasks-*-jar-with-dependencies.jar | head -n 1)
 
